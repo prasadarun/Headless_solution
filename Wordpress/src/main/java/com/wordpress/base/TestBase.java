c7cd933bfd6a9ae0d450885f8858034d4dcee1ca
prasad <prasadcrazyboy777@gmail.com>
2019-05-26 11:10:38 +0530

    Added Chrome and firefox in unit driver

--- Wordpress/src/main/java/com/wordpress/base/TestBase.java
+++ Wordpress/src/main/java/com/wordpress/base/TestBase.java
@@ -11,9 +11,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.wordpress.utils.LoadingTime;
 
 public class TestBase {
+
 
 	public static WebDriver driver;
 	public static Properties prop;
@@ -43,29 +46,46 @@ public class TestBase {
 		System.out.println("Selected:"+ Type_of_run);
 
 		if(Type_of_run.equals("headless")){
-			driver = new HtmlUnitDriver();
-			
-		}
-		
-		else if(Type_of_run.equals("head")){
-			
-		if(browserName.equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver", "D:\\Lib for Testing tools\\Drivers\\chromedriver_win32\\chromedriver.exe");
-			driver = new ChromeDriver();	
-		}
-		else if(browserName.equals("Firefox")){
-			System.setProperty("webdriver.gecko.driver", "C:\\Users\\dpindi.CORP\\Desktop\\Selenium practice\\Drivers.exe files\\geckodriver.exe");
-			driver = new FirefoxDriver();	
+
+			if(browserName.equals("chrome")){
+				driver = new HtmlUnitDriver(BrowserVersion.CHROME);
+				System.out.println(BrowserVersion.CHROME);
+			}
+
+
+			else if(browserName.equals("Firefox")) {
+				driver = new HtmlUnitDriver(BrowserVersion.FIREFOX_60);
+			}
+
+
+			else if (browserName.equals("nill")){
+				driver = new HtmlUnitDriver();
+			}
+
+
+
+
 		}
 
-		else {
-			System.out.print("No other browsers are defined");
-		}
-		
-		driver.manage().window().maximize();
-		driver.manage().timeouts().pageLoadTimeout(LoadingTime.PAGE_LAYOUT, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(LoadingTime.IMPLICT_WAIT, TimeUnit.SECONDS);
-		
+		else if(Type_of_run.equals("head")){
+
+			if(browserName.equals("chrome")) {
+				System.setProperty("webdriver.chrome.driver", "D:\\Lib for Testing tools\\Drivers\\chromedriver_win32\\chromedriver.exe");
+				driver = new ChromeDriver();	
+			}
+			else if(browserName.equals("Firefox")){
+				System.setProperty("webdriver.gecko.driver", "C:\\Users\\dpindi.CORP\\Desktop\\Selenium practice\\Drivers.exe files\\geckodriver.exe");
+				driver = new FirefoxDriver();	
+			}
+
+			else {
+				System.out.print("No other browsers are defined");
+			}
+
+			driver.manage().window().maximize();
+			driver.manage().timeouts().pageLoadTimeout(LoadingTime.PAGE_LAYOUT, TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(LoadingTime.IMPLICT_WAIT, TimeUnit.SECONDS);
+
 		}
 
 		driver.get(prop.getProperty("url"));
